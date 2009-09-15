4592e290e50670b61d24e86988f6159d9b80607d
Rod Whitby <rod@whitby.id.au>
2009-09-15 20:39:55 +0930

    libpurpleAdapter.c includes a file called lunaservice.h

---
+++ lunaservice.h
@@ -0,0 +1,8 @@
+/*
+ * lunaservice.h
+ */
+
+/* The existence of this header file is disclosed at line 38 of
+   LibpurpleAdapter.c, a file licensed under the LGPL 2.1, from
+   http://palm.cdnetworks.net/opensource/1.1.0/libpurple-adapter-1.0.tgz */
+
